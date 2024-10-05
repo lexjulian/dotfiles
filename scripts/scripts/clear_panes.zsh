3545d9a53a0a08ee36772c93fad7e74c4dc756c6
lexjulian <alexanderjohnjulian@gmail.com>
2024-10-05 22:12:36 +0800

    preping for device backup

--- scripts/scripts/clear_panes.zsh
+++ scripts/scripts/clear_panes.zsh
@@ -3,7 +3,7 @@
 for pane in $(tmux list-panes -F '#P'); do
   if [ "$pane" -eq 0 ]; then
     tmux select-pane -t $pane
-    tmux send-keys 'clearf' C-m
+    tmux send-keys 'cf' C-m
   else
     tmux select-pane -t $pane
     tmux send-keys 'clear' C-m
