6ee37b3a6bad81134895de28cf25334d93dedddd
lexjulian <alexanderjohnjulian@gmail.com>
2024-12-18 19:54:52 +0800

    added formatter for python

--- nvim_main/.config/nvim_main/lua/alexj/plugins/conform.lua
+++ nvim_main/.config/nvim_main/lua/alexj/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
             formatters_by_ft = {
                 lua = { "stylua" },
                 javascript = { "prettierd", "prettier", stop_after_first = true },
+                python = { "black" },
             },
         })
 
