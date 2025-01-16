58e0797428034e210eefd594cc4201c1f6ecfaab
lexjulian <alexanderjohnjulian@gmail.com>
2025-01-16 14:00:31 +0800

    updated to new file name

--- scripts/.config/scripts/datetime.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script will notify the date and time. You can bind this to a key available for you.
-date_time=$(date +"%A, %B %d, %Y %I:%M %p")
-
-dunstify "$date_time" -u normal
